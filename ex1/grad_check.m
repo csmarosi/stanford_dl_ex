f9ac76a7edc60d4de6aa8e4419b6d5dec29dfab5
Csaba Marosi <cs.marosi@gmail.com>
2015-06-17 17:43:11 +0000

    Better debug code; print only the errors

--- ex1/grad_check.m
+++ ex1/grad_check.m
@@ -3,7 +3,7 @@ function average_error = grad_check(fun, theta0, num_checks, varargin)
   delta=1e-3; 
   sum_error=0;
 
-  fprintf(' Iter       i             err');
+  fprintf(' Iter       j             err');
   fprintf('           g_est               g               f\n')
 
   for i=1:num_checks
@@ -19,10 +19,13 @@ function average_error = grad_check(fun, theta0, num_checks, varargin)
     g_est = (f1-f0) / (2*delta);
     error = abs(g(j) - g_est);
 
+    if (error>1e-5)
     fprintf('% 5d  % 6d % 15g % 15f % 15f % 15f\n', ...
             i,j,error,g(j),g_est,f);
+    size(g)
+    end
 
     sum_error = sum_error + error;
   end
 
-  average=sum_error/num_checks;
+  average_error=sum_error/num_checks;
